0a3d8615f5af3b604286be45f441066cf53eb9e5
codermoderSD <shubham.dalvi008@gmail.com>
2025-12-28 18:03:42 +0530

    fix: config changes

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/vidbit-poc",
-  assetPrefix: "/vidbit-poc/",
   images: {
     unoptimized: true,
   },
